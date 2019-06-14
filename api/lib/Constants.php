752bed95aef1b5697aeb899dd7c797ae91c36533
Rafat Hossain <rafathossain96@gmail.com>
2019-06-14 21:49:28 +0600

    Updated bugs

--- api/lib/Constants.php
+++ api/lib/Constants.php
@@ -1,8 +1,8 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_USERNAME', '');
-define('DB_PASSWORD', '');
-define('DB_NAME', '');
+define('DB_USERNAME', 'YOUR_USERNAME_HERE');
+define('DB_PASSWORD', 'YOUR_PASSWORD_HERE');
+define('DB_NAME', 'YOUR_DATABASE_NAME_HERE');
 
 ?>
--- api/lib/StatusCode.php
+++ api/lib/StatusCode.php
@@ -4,12 +4,9 @@ define('S1000',"Process completed successfully for the requested operation.");
 
 define('E1000',"Request not allowed with the method you are trying.");
 define('E1001',"Parameters are missing.");
-define('E1002',"Sorry. No data available.");
 define('E1003',"Invalid email address.");
-define('E1004',"Invalid date. Date format: YYYY-MM-DD.");
 define('E1005',"Invalid password.");
 define('E1006',"No user found.");
 define('E1007',"Password mismatch.");
-define('E1008',"Invalid order id.");
 
 ?>
